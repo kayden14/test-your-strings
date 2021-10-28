03ecb43fda709b99d6b6d25d2bab59a7bcf6266a
kayden14 <faithtega14@gmail.com>
2021-10-28 15:36:46 +0100

    created js file

---
+++ js/loop2.js
@@ -0,0 +1,22 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: '
+
+ let i = 0;
+ for( ; i < people.length; i++){
+ if(people[i] === 'Phil' || people[i] === 'Lola') {
+ refused.textContent += people[i] + ', ';
+ } else {
+ admitted.textContent += people[i] + ', ';
+ }
+}
+
+refused.textContent = refused.textContent.slice(0, refused.textContent.length-2);
+refused.textContent = refused.textContent + '.';
+
+admitted.textContent = admitted.textContent.slice(0, admitted.textContent.length-2);
+admitted.textContent = admitted.textContent + '.';
+
