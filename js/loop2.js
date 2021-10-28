3b2686761c91d1570686760b7bed772cf76b78b0
kayden14 <faithtega14@gmail.com>
2021-10-28 16:19:47 +0100

    modified my js

--- js/loop2.js
+++ js/loop2.js
@@ -20,3 +20,20 @@ refused.textContent = refused.textContent + '.';
 admitted.textContent = admitted.textContent.slice(0, admitted.textContent.length-2);
 admitted.textContent = admitted.textContent + '.';
 
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: '
+
+ let i = 0;
+ for( ; i < people.length; i++){
+ if(people[i] === 'Phil' || people[i] === 'Lola') {
+ refused.textContent += people[i] + ', ';
+ } else {
+ admitted.textContent += people[i] + ', ';
+ }
+}
+admitted.textContent = admitted.textContent.slice(0, admitted.textContent.lastIndexOf(',')) + '.'
+refused.textContent = refused.textContent.slice(0, refused.textContent.lastIndexOf(',')) +  '.'
