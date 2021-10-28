21a1b0d67cee0e305bbe41655fbd8cf8e4fba8a4
kayden14 <faithtega14@gmail.com>
2021-10-28 13:06:39 +0100

    created html files and js files

---
+++ js/tutor.js
@@ -0,0 +1,81 @@
+var myName;
+let myDateOfBirth;
+const state_of_origin = "delta";
+
+myName = "faith";
+myDateOfBirth = '31st of october,2004';
+let myGender = 'female'
+let myBestColor = 'black and ash';
+let myBestFood = 'spagetti';
+let myHobbies = 'listening to music and reading novels';
+let myBestFriend = 'sam';
+let myComplexion = 'medium';
+let myBestMusic = 'alcohol';
+
+console.log(myName);
+console.log(myDateOfBirth);
+console.log(myGender);
+console.log(myBestColor);
+console.log(myBestFood);
+console.log(myBestFriend);
+console.log(myHobbies);
+console.log(myComplexion);
+console.log(myBestMusic);
+
+let myAge = Number(prompt('how old are you'));
+console.log(myAge);
+let currentYear = 2021;
+console.log(currentYear);
+typeof currentYear
+console.log(typeof currentYear);
+typeof myAge
+console.log(typeof myAge);
+let yearOfBirth = currentYear - myAge;
+console.log(yearOfBirth);
+myGender.indexOf()
+
+
+
+
+
+
+// math1
+let num1 = 14;
+let num2 = 10;
+let num3 = 6;
+let num4 = 8;
+let num5 = num1 + num2;
+let num6 = num4 - num3;
+finalResult = num5 * num6;
+
+
+
+// math2
+let result = (7 + 13) /( 9 + 7);
+let result2 = 100 / (2 * 6);
+result *= result2;
+let finalResult = result.toFixed(2);
+typeof finalResult;
+let finalNumber = Number( finalResult );
+
+
+
+// math3
+// statement 1: The elephant weighs more mouse
+let eleWeight = 1000;
+let mouseWeight = 2;
+let weightComparison = eleWeight < mouseWeight;
+
+
+// Statement 2: The Ostrich is taller than the duck
+let ostrichHeight = 2;
+let duckHeight = 0.3;
+let heightComparison = ostrichHeight > duckHeight;
+
+// Statement 3: The two passwords match
+let pwd1 = 'stromboli';
+let pwd2 = 'stROmBoLi';
+let pwdMatch = pwd1 !== pwd2;
+
+
+
