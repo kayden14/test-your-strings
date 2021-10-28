ca6c1f17b9006f548054c2314b31b8e84f6ee2ea
kayden14 <faithtega14@gmail.com>
2021-10-28 13:03:19 +0100

    created js file

---
+++ js/tys1.js
@@ -0,0 +1,29 @@
+const list = document.querySelector('.output ul');
+const totalBox = document.querySelector('.output p');
+let total = 0;
+list.innerHTML = '';
+totalBox.textContent = '';
+// number 1
+let products = [
+                'Underpants:6.99',
+                'Socks:5.99',
+                'T-shirt:14.99',
+                'Trousers:31.99',
+                'Shoes:23.99'];
+
+for (let i = 0; i <products.length; i++) { // number 2
+  // number 3
+	let eachItem = products[i].split(':');
+	let price = Number(eachItem[1]);
+  // number 4
+	total += price
+  // number 5
+  let itemText = `${eachItem[0]} - $${price}`;
+
+  const listItem = document.createElement('li');
+  listItem.textContent = itemText;
+  list.appendChild(listItem);
+}
+
+totalBox.textContent = 'Total: $' + total.toFixed(2);
+
